bf6774fd5146dd229f3df5b3be4b4f9abef23f05
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2025-09-23 14:37:21 +0300

    Update passpdf.py

--- passpdf.py
+++ passpdf.py
@@ -1,6 +1,6 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import getpass
-# check python 
+# check python  dsdss
 # Making an instance of the PdfFileWriter class and storing it in a variable ewewew
 writer = PdfFileWriter()
 # xaaxax dsdsd
