3f747ac9b749163e7f05637ae10e04d86420653d
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2025-09-23 14:34:41 +0300

    test python subanalyzer

--- passpdf.py
+++ passpdf.py
@@ -3,7 +3,7 @@ import getpass
 # check python 
 # Making an instance of the PdfFileWriter class and storing it in a variable ewewew
 writer = PdfFileWriter()
-# xaaxax
+# xaaxax dsdsd
 # wqwqwqwq
 # Explicitly ask the user what the name of the original file is
 pdf_name = input('Pleast type in the name of the pdf file suffixed with its extention: ')
